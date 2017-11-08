9c303a3d754eaef5cb4af6d9b18351cc53f977e1
Carter King <carterking002@gmail.com>
2017-11-07 19:17:36 -0600

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    console.log('We are ready!');
+
+    $('.enter-left').addClass('hidden').viewportChecker({
+        classToAdd: 'visible animated fadeInLeft',
+        offset: 100
+    });
+
+    $('.enter-right').addClass('hidden').viewportChecker({
+        classToAdd: 'visible animated fadeInRight',
+        offset: 100
+    });
+    
+    $('.slick-carousel').slick({
+        infinite: true,
+        speed: 1000,
+        autoplay: true,
+        fade: true,
+        cssEase: 'linear',
+        adaptiveHeight: true,
+    });
+
+
+});
